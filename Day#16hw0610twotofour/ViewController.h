5c5dc167a33cb76d65a15b1b62841272d0dab704
Evan <evantsaip@gmail.com>
2015-06-10 21:58:27 +0800

    First commit

---
+++ Day#16hw0610twotofour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day#16hw0610twotofour
+//
+//  Created by lalaleelala on 6/10/15.
+//  Copyright (c) 2015 lalaleelala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Day#16hw0610twotofourTests/Day_16hw0610twotofourTests.m
@@ -0,0 +1,40 @@
+//
+//  Day_16hw0610twotofourTests.m
+//  Day#16hw0610twotofourTests
+//
+//  Created by lalaleelala on 6/10/15.
+//  Copyright (c) 2015 lalaleelala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Day_16hw0610twotofourTests : XCTestCase
+
+@end
+
+@implementation Day_16hw0610twotofourTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
